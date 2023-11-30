0050318c19a125a4ec84297d3c8e5bc7b5b66b13
Abdo-Salman <149829707+Abdo-Salman@users.noreply.github.com>
2023-11-30 10:16:00 +0200

    Added process.py library and main.py and modified the names of the folders
    =process.py library contains all filters
      in form of functions
    
    =main.py for main execution of the
       program
    
    =names of the folders are the only
      change the files inside are the same

--- filters/crop/crop.py
+++ filters/crop/crop.py
@@ -3,9 +3,10 @@ import imutils
 import cv2
 from matplotlib import pyplot as plt
 
+
 image = cv2.imread('Y4.jpg')
 
-    
+        
 # Convert the image to grayscale, and blur it slightly
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (5, 5), 0)
@@ -20,19 +21,17 @@ thresh = cv2.dilate(thresh, None, iterations=2)
 cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
 c = max(cnts, key=cv2.contourArea)
-    
+        
 
 # Find the extreme points
 extLeft = tuple(c[c[:, :, 0].argmin()][0])
 extRight = tuple(c[c[:, :, 0].argmax()][0])
 extTop = tuple(c[c[:, :, 1].argmin()][0])
 extBot = tuple(c[c[:, :, 1].argmax()][0])
-    
+        
 # crop new image out of the original image using the four extreme points (left, right, top, bottom)
 new_image = image[extTop[1]:extBot[1], extLeft[0]:extRight[0]]            
 
-cv2.imwrite("crop.jpg",new_image)
-
-
-
-
+cv2.imshow("crop.jpg",new_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
